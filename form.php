89d7b1c016921d17bf9d5cc4bfb7c1d6e811da8c
Ali <ali@MacBook-Air-de-Ali.local>
2022-06-24 13:44:57 +0200

    PHP_Challenge_9

--- form.php
+++ form.php
@@ -1,63 +1,67 @@
-<?php
-$Sujets = array( "Choix1" => "Choix N°1", "Choix2" => "Choix N°2", "Choix3" => "Choix N°3");
-?>
-
-<form  action="thanks.php" method="post">
-
-    <div>
-      <label for="name">Nom :</label>
-      <input type="text"  id="name"  name="user_name">
-    </div>
-
-    <div>
-      <label for="firstname">Prénom :</label>
-      <input type="text"  id="firstname"  name="user_firstname">
-    </div>
-
-    <div>
-      <label  for="email">Courriel :</label>
-        <input  type="email"  id="email"  name="user_email">
-    </div>
-
-    <div>
-      <label for="phone">Portable :</label>
-      <input type="tel"  id="phone"  name="user_phone">
-    </div>
-
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div id="conteneur">
+
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+
+  <body>
+
+    <?php
+      $Sujets = array( "Choix1" => "Choix N°1", "Choix2" => "Choix N°2", "Choix3" => "Choix N°3");
+    ?>
+
+    <form  action="thanks.php" method="post">
+
+      <div>
+        <label for="name">Nom :</label>
+        <input type="text"  id="name"  name="user_name">
+      </div>
+
+      <div>
+        <label for="firstname">Prénom :</label>
+        <input type="text"  id="firstname"  name="user_firstname">
+      </div>
+
+      <div>
+        <label  for="email">Courriel :</label>
+        <input  type="email"  id="email"  name="user_email">
+      </div>
+
+      <div>
+        <label for="phone">Portable :</label>
+        <input type="tel"  id="phone"  name="user_phone">
+      </div>
+
+
+      <div id="conteneur">
         <h4>Choisir un sujet</h4>
-        <select name="choix">
-           
-            <?php
-           foreach( $Sujets as $k=>$v){
-               echo "<option value=\"$k\">$v</option>"; 
-           } 
-           ?> 
-
+        <select name="choix">       
+          <?php
+            foreach( $Sujets as $k=>$v){
+            echo "<option value=\"$k\">$v</option>"; 
+            } 
+          ?> 
         </select>
-    </div>
-</body>
+      </div>
+
+      <div>
+        <label  for="message">Message :</label>
+        <textarea  id="message"  name="user_message"></textarea>
+      </div>
+
+      <div  class="button">
+        <button  type="submit">Envoyer votre message</button>
+      </div>
+        
+    </form>
+
+  </body>
+
 </html>
-
-    <div>
-      <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
-    </div>
-
-    <div  class="button">
-      <button  type="submit">Envoyer votre message</button>
-    </div>
-    
-</form>
 
 
     
--- thanks.php
+++ thanks.php
@@ -4,33 +4,33 @@ $Sujets = array( "Choix1" => "Choix N°1", "Choix2" => "Choix N°2", "Choix3" =>
 $user_name = $_POST['user_name'];
 $user_firstname = $_POST['user_firstname'];
 $Sujet = $_POST['choix'];
-$user_email = $_POST['user_email']; //filter_var($_POST['user_email'], FILTER_SANITIZE_EMAIL);
+$user_email = $_POST['user_email']; 
 $user_phone = $_POST['user_phone'];
 $message = $_POST['user_message'];
-$err = '';
+$erreur = '';
 
 if (empty($user_name)) {
-    $err .= "name is required. ";
+    $erreur .= "name is required. ";
 }
 if (empty($user_firstname)) {
-    $err .= "Firstname is required. ";
+    $erreur .= "Firstname is required. ";
 }
 if (empty($user_email)) {
-    $err .= "Email is required. ";
+    $erreur .= "Email is required. ";
 } elseif (filter_var($user_email, FILTER_VALIDATE_EMAIL) === false) {
-    $err .= "Email is not valid. ";
+    $erreur .= "Email is not valid. ";
 }
 if (empty($user_phone)) {
-    $err .= "Telephone is required. ";
+    $erreur .= "Telephone is required. ";
 }
 if (empty($message)) {
-    $err .= "Message is required. ";
+    $erreur .= "Message is required. ";
 }
 
-if (empty($err)) {
+if (empty($erreur)) {
     echo 'Merci ', $user_name, ' ', $user_firstname, ' de nous avoir contacté à propos de ', $Sujet,'.<br>Un de nos conseillers vous contactera soit à l’adresse ', $user_email, ' ou par téléphone au ', $user_phone, ' dans les plus brefs délais pour traiter votre demande: <br>', $message;
 } else {
-    echo $err;
+    echo $erreur;
 }
 
 ?>
